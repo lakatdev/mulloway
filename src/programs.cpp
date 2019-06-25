a024b8b52dcf65e4562b6102be8b860f9bb78401
Robert Fodor <fodor@debian-e403sa>
2019-06-25 20:55:04 +0200

    hotfix

--- src/programs.cpp
+++ src/programs.cpp
@@ -7,7 +7,7 @@ static int runningProgram;
 
 void exit(){
     runningProgram = 0;
-    common::setHeader("Mulloway Acorn: 1.18", false);
+    common::setHeader("Mulloway Acorn: 1.18b", false);
     common::clear();
     common::print("Installed applications:\n -Text editor (edit)", 0,0);
 }
@@ -21,9 +21,6 @@ class Prog_textEditor {
         void main(){
             common::setHeader("Text editor", true);
             common::print("<<", 78, pointer);
-			graphics::enable(41);
-			graphics::drawString(100,0,"please let me eat","light");
-			graphics::drawString(100,100,"hi my name is slim shady","dark");
         }
         void inpt(string com, int key = 0){
             if (key == 1 || key == 2){
