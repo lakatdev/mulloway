4b68fd9d7b11f53be9c15ab50ed773891f9ba9d6
LovasR <46626797+LovasR@users.noreply.github.com>
2019-09-15 21:08:27 +0200

    Added simple math functions

---
+++ include/math.h
@@ -0,0 +1,9 @@
+#ifndef MATH_H
+#define MATH_H
+
+#include <types.h>
+
+int power(int base, int n);
+int abs(int n);
+
+#endif
---
+++ src/math.c
@@ -0,0 +1,11 @@
+#include <math.h>
+
+int power(int base, int n){
+    int p;
+    for(p = 1; n > 0; --n)
+        p = p * base;
+    return p;
+}
+int abs(int n){
+    return ((n<0)?(-n):(n));
+}
